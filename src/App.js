ad615975c8e5f4d12aa46a4a3144401e22583cd3
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-03 14:19:04 -0500

    created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import NavBar from './components/NavBar/NavBar.js'
+import About from './components/About/About.js'
+import Home from './components/Home/Home.js'
+import Projects from './components/Projects/Projects.js'
+import Contact from './components/Contact/Contact.js'
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar/>
+      <About/>
+      <Home/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return ( 
+        <div>
+           <h1>Navigation Bar</h1> 
+        </div>
+     );
+}
+ 
+export default NavBar;
