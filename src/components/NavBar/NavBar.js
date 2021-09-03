fa8d5c2473d655cafa18d2894664430d2141e78d
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-03 14:24:41 -0500

    updated navbar component with nav links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,22 @@
 const NavBar = () => {
     return ( 
         <div>
-           <h1>Navigation Bar</h1> 
+           <nav>
+               <ul>
+                   <li>
+                       <a href="/">Home</a>
+                   </li>
+                   <li>
+                       <a href="/#about">About</a>
+                   </li>
+                   <li>
+                       <a href="/#projects">Projects</a>
+                   </li>
+                   <li>
+                       <a href="/#contact">Contact</a>
+                   </li>
+               </ul>
+            </nav> 
         </div>
      );
 }
