f59b02a11b5816bd2a159234440559deffda8d21
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-03 23:28:54 -0500

    added styling to about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,24 @@
 import classes from './About.module.css'
 import PageHeader from './../PageHeader/PageHeader.js'
+import me from './../../img/me.jpg'
 
 const About = () => {
     return ( 
-        <div>
+        <div className={classes.AboutMe}>
             <PageHeader title={'About Me'}/>
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Hello! I am Danielle</h2>
+                    <p>
+                        I am a Software Engineer based in Dallas.
+                        Duis consectetur qui qui laborum aliquip esse amet commodo sunt cupidatat consequat quis. Velit deserunt nostrud exercitation pariatur officia magna anim. Id fugiat enim sit magna id officia consectetur. Velit ut incididunt tempor cupidatat laboris nisi eiusmod nostrud aute id ullamco ea. Nisi elit dolor deserunt laboris aute aliquip.
+                        Aute cupidatat id proident irure. Amet pariatur consequat commodo sunt id aliquip aute amet do. Irure occaecat sunt dolor aliqua Lorem do sit officia. Ad commodo aute ut magna commodo nostrud nisi occaecat culpa eiusmod dolore ipsum. Sunt quis culpa non in magna consequat pariatur incididunt officia sunt fugiat nostrud dolore qui.
+                    </p>
+                </div>
+                <div className={classes.Photo}>
+                    <img className={classes.Me} src={me} alt="me"></img>
+                </div>
+            </div>
         </div>
      );
 }
