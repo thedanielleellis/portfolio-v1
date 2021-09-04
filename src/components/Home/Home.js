992eeebbf393c4b35766f89bdc0dcf8e3337cbe9
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-03 22:49:52 -0500

    added text and style to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,17 @@
+import classes from './Home.module.css'
+import code from './../../img/code.png'
+import code2 from './../../img/code2.png'
+
+
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home</h1>
+        <div className={classes.Home}>
+            <div className={classes.Container}>
+                <h1 className={classes.Hello}>Hello, world.</h1>
+                <h1>Welcome to my website</h1>
+            </div>
+            <img className={classes.Code} src={code} alt="code"></img>
+            <img className={classes.Code2} src={code2} alt="code2"></img>
         </div>
      );
 }
