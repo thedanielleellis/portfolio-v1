e579af6e34f8f559d9d2a3c68b8677d39a758e14
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-03 23:46:53 -0500

    added style to Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,20 @@
+import classes from './Projects.module.css'
+import PageHeader from './../PageHeader/PageHeader.js'
+import code from './../../img/code.png'
+
+
+
 const Projects = () => {
     return ( 
-        <div>
-            <h1>Projects</h1>
+        <div className={classes.Projects}>
+            <PageHeader title={'My Work'}/>
+            <p>
+                I'm a life long learner and enjoy learning new stuff! Below are some programming languages, toolkits, frameworks and librabries that I worked with and like. 
+            </p>
+            <div className={classes.Container}>
+                <img src={code} alt="code"></img>
+
+            </div>
         </div>
      );
 }
