f9bc8a1d87905c53e10f1af1142881d4abf359ec
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-05 17:53:47 -0500

    update home page styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,25 @@ import classes from './Home.module.css'
 import circle from './../../img/circle.png'
 import headshot from './../../img/headshot.png'
 
-
-
-
 const Home = () => {
-    return ( 
+    return (
         <div className={classes.Home} id="home">
             <div className={classes.Container}>
-                <h1 className={classes.Hello}>Hello, world.</h1>
+                <h1 className={classes.Welcome}>Hello, world.</h1>
                 <h1>Welcome to my website</h1>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+
+                <p>Danielle Ellis, MBA</p>
+                <p>Software Engineer</p>
             </div>
-            <img className={classes.Headshot} src={headshot} alt="headshot"></img>
             <img className={classes.Circle} src={circle} alt="circle"></img>
+            <img className={classes.Headshot} src={headshot} alt="headshot"></img>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
