eb65ca6ea7e2a2f278e39988ab33ab5b274c9334
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-09 13:00:45 -0500

    refactored projects model to be more user-friendly

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                         I am a creative Software Engineer based in Dallas, TX with a passion for applying new cutting-edge technologies to enhance the innovative atmosphere of a business. 
                     </p>
                     <p>
-                        My interest in web development started in 2016 when I built my very first business website using custom themes and editing the html and css. After then, I began building websites as a freelancer part-time. 
+                        My interest in web development started in 2016 when I built my very first business website using custom themes and editing the html and css. After that, I created websites for family and friends then began as a freelancer part-time. 
                     </p>
                     <p>     
                         In the last 3 years, I've had the privilege of working as an Executive Admin Assistant where I worked hand-in-hand with the Software Engineering Department to review and give feedback for healthcare database structures and I designed custom marketing materials such as global weekly newsletters, email campaigns, and websites.
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,8 +5,84 @@ import hiddengems from './../../img/hiddengems.png'
 import virtualdance from './../../img/virtualdance.png'
 import { motion } from 'framer-motion'
 
+const listTitleStyle = { fontWeight: 900, color: '#b45118', marginBottom: '4px', fontSize: '18px'}
 
 
+const project1 =
+    <div>
+        <a href="https://dev.to/thedanielleellis"><img src={portfolio} alt="portfolio project" target="_blank" rel="noreferrer"></img></a>
+        <ul>
+            <li style={listTitleStyle}>Portfolio Website Early Version</li>
+            <li>Ruby on Rails API - React/Redux</li>
+            <li>JavaScript - Bootstrap</li>
+
+        </ul>
+    </div>
+
+const project2 =
+    <div>
+        <a href="https://dev.to/thedanielleellis"><img src={hiddengems} alt="hidden gems project" target="_blank" rel="noreferrer"></img></a>
+        <ul>
+            <li style={listTitleStyle}>Hidden Gems Mentorship</li>
+            <li>Ruby on Rails - JavaScript</li>
+
+
+        </ul>
+    </div>
+
+const project3 =
+    <div>
+        <a href="https://dev.to/thedanielleellis"><img src={virtualdance} alt="virtual dance project" target="_blank" rel="noreferrer"></img></a>
+        <ul>
+            <li style={listTitleStyle}>Virtual Dance Machine</li>
+            <li>Ruby on Rails - PostgreSQL - JavaScript</li>
+            <li>Bootstrap - ActiveRecord</li>        
+        </ul>
+    </div>
+
+const totalProjects = [project1, project2, project3]
+
+
+const Projects = () => {
+    return (
+        <div className={classes.Projects} >
+            <PageHeader title={'Featured Work'} />
+            <div className={classes.TechDesc}>
+                <p>Technologies I've been working with recently:</p>
+                <ul>
+                    <li>Javascript</li>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>Node.js</li>
+                    <li>Ruby</li>
+                    <li>Rails</li>
+                    <li>PostgreSQL</li>
+                </ul>
+            </div>
+            <div className={classes.Container}>
+                {totalProjects.map(projects => {
+                    return (
+                        <motion.div className={classes.List}
+                            whileHover={{
+                                scale: 1.1,
+                                textShadow: "0px 0px 8px rgb(255, 255, 255)",
+                                boxShadow: "0pz 0pz 8px rgb(255, 255, 255)"
+                            }}>
+                            {projects}
+                        </motion.div>
+                    )
+                })}
+            </div>
+
+        </div>
+    );
+}
+
+export default Projects;
+
+
+
+/*
 const Projects = () => {
     return (
         <div className={classes.Projects} id="projects">
@@ -83,4 +159,5 @@ const Projects = () => {
     );
 }
 
-export default Projects;
+
+export default Projects; */
