5a632f64e46ea0c3a4073c99616c1b435d008ea6
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-09 13:07:20 -0500

    added project id for navbar

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,7 +45,7 @@ const totalProjects = [project1, project2, project3]
 
 const Projects = () => {
     return (
-        <div className={classes.Projects} >
+        <div className={classes.Projects} id="projects">
             <PageHeader title={'Featured Work'} />
             <div className={classes.TechDesc}>
                 <p>Technologies I've been working with recently:</p>
@@ -79,85 +79,3 @@ const Projects = () => {
 }
 
 export default Projects;
-
-
-
-/*
-const Projects = () => {
-    return (
-        <div className={classes.Projects} id="projects">
-            <div className={classes.PageHeader}>
-                <PageHeader title={'Featured Work'} />
-            </div>
-            <div className={classes.TechDesc}>
-                <p>Technologies I've been working with recently:</p>
-                    <ul>
-                        <li>Javascript</li>
-                        <li>React</li>
-                        <li>Redux</li>
-                        <li>Node.js</li>
-                        <li>Ruby</li>
-                        <li>Rails</li>
-                        <li>PostgreSQL</li>
-                    </ul>
-            </div>
-            <div className={classes.Container}>
-                <div className={classes.PortfolioRow}>
-                    <div className={classes.PortfolioWrapper}>
-                        <motion.div className={classes.PortfolioItem}
-                            whileHover={{
-                                scale: 1.1,
-                                textShadow: "0px 0px 8px rgb(255, 255, 255)",
-                                boxShadow: "0pz 0pz 8px rgb(255, 255, 255)"
-                            }}
-                        >
-                            <div className={classes.ImageLink}>
-                                <a href="https://dev.to/thedanielleellis"><img src={portfolio} alt="portfolio project" target="_blank" rel="noreferrer"></img></a>
-                                <div className={classes.PortfolioInfo}>
-                                    <h3>Portfolio Website Early Version</h3>
-                                    <h4>Ruby on Rails API, React/Redux, JavaScript, HTML/CSS, Bootstrap</h4>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </div>
-                    <div className={classes.PortfolioWrapper}>
-                        <motion.div className={classes.PortfolioItem}
-                        whileHover={{
-                            scale: 1.1,
-                            textShadow: "0px 0px 8px rgb(255, 255, 255)",
-                            boxShadow: "0pz 0pz 8px rgb(255, 255, 255)"
-                        }}>
-                            <div className={classes.ImageLink}>
-                                <a href="https://dev.to/thedanielleellis"><img src={hiddengems} alt="hidden gems project" target="_blank" rel="noreferrer"></img></a>
-                                <div className={classes.PortfolioInfo}>
-                                    <h3>Hidden Gems Mentorship</h3>
-                                    <h4>Ruby on Rails, PostgreSQL, JavaScript, HTML/CSS, Bootstrap, ActiveRecord</h4>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </div>
-                    <div className={classes.PortfolioWrapper}>
-                        <motion.div className={classes.PortfolioItem}
-                        whileHover={{
-                            scale: 1.1,
-                            textShadow: "0px 0px 8px rgb(255, 255, 255)",
-                            boxShadow: "0pz 0pz 8px rgb(255, 255, 255)"
-                        }}>
-                            <div className={classes.ImageLink}>
-                                <a href="https://dev.to/thedanielleellis"><img src={virtualdance} alt="virtual dance project" target="_blank" rel="noreferrer"></img></a>
-                                <div className={classes.PortfolioInfo}>
-                                    <h3>Virtual Dance Machine</h3>
-                                    <h4>Ruby on Rails, JavaScript, HTML/CSS</h4>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    );
-}
-
-
-export default Projects; */
